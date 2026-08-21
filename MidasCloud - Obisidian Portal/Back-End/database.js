34a970491d8ac9b3857a7660f5ade3f7edab74b8
NateTheGrappler <guzinskinatan@gmail.com>
2026-08-21 04:32:14 +0000

    Basically just a VERY small database set up so I can actually move ahead with the project

---
+++ MidasCloud - Obisidian Portal/Back-End/database.js
@@ -0,0 +1,22 @@
+const DATA_BASE = require('better-sqlite3'); //import sqlite 
+const database = new DATA_BASE('portal.db'); //opens connection to database on app startup
+
+//this basically creates the table inside of the given database object
+//it runs every single time this file does, and only actually creates the table if
+//it does not exist, it populates the general status of the table with needed fields
+//for functionality
+database.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL,
+    password_hash TEXT,
+    hasDefaultPwd INTEGER CHECK (hasDefaultPwd IN (0, 1)),
+    status TEXT NOT NULL DEFAULT 'pending',
+    vaultLink TEXT,
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+    `);
+
+
+module.exports = database;
