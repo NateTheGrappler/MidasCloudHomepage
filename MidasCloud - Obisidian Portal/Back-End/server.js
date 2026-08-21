e925f76d9f5cd93ac249c41e3387e2fe2f2ba912
NateTheGrappler <guzinskinatan@gmail.com>
2026-08-21 04:10:39 +0000

    set up just basic flow for how the backend is gonna work

--- MidasCloud - Obisidian Portal/Back-End/server.js
+++ MidasCloud - Obisidian Portal/Back-End/server.js
@@ -2,13 +2,28 @@ const express = require('express') //import express lib from node
 const app = express(); //actually set up the `app` object for handling requests and stufff
 
 
-//personal note: req = request, res = response
-app.get('/', (req, res) => {
-    res.send('HELLO WORLD!');
-});
+//middleware to handle json data from incoming login data
+app.use(express.json());
+
+app.post('/api/login', (req, res) => {
+
+    //set get the username and password from what was passed in,
+    //the reason you can use it as the json data is because of the middleware
+    const {username, password} = req.body;
+
+    
+
+    res.json({username : username, password : password});
+})
 
 
 app.listen(3000, () => {
     //first line whenever this bad boy runs
     console.log ("Midas Cloud Server.js is now running! \n");
 });
+
+
+
+
+
+//personal note: req = request, res = response
