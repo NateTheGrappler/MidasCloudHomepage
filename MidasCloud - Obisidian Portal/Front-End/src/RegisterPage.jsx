a9c56cdf17142ba6c8d87e3fb569c26215dcb579
Natan Guzinski <guzinskinatan@gmail.com>
2026-09-02 22:55:16 -0400

    BUG FIX: added in same .env URL handling to route file as other files

--- MidasCloud - Obisidian Portal/Front-End/src/RegisterPage.jsx
+++ MidasCloud - Obisidian Portal/Front-End/src/RegisterPage.jsx
@@ -61,7 +61,7 @@ function RegisterPage()
         try
         {
             //get data and parse it
-            const response = await fetch("http://localhost:3000/api/register", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
